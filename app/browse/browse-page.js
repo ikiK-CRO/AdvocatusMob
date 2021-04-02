5a1ef4d2dc881b03ef95f436803e4972028daa6a
Kiki <kiki.ikik@gmail.com>
2021-04-02 23:59:35 +0200

    gumb tap true

--- app/browse/browse-page.js
+++ app/browse/browse-page.js
@@ -2,24 +2,78 @@ import { Application } from '@nativescript/core'
 
 import { BrowseViewModel } from './browse-view-model'
 
-import { EventData, fromObject, ListPicker, Page } from '@nativescript/core'
+import { Label, GestureTypes } from '@nativescript/core'
 
-export function onNavigatingTo(args) {
+export function onNavigatingTo (args) {
   const page = args.object
   page.bindingContext = new BrowseViewModel()
-  page.bindingContext.set('years', years)
+
+  page.bindingContext.set('pretraga', pretraga)
+  page.bindingContext.set('rezultat', getMessage(val))
+
+  let btn = page.getViewById('btn')
+
+  btn.on(GestureTypes.tap, function (args) {
+    console.log('Tap')
+    let res = page.getViewById('val').text
+    let url = 'https://advocatus-test.appdiz-informatika.hr/api/api_V1.php'
+    let req = url + '?apitest=' + res
+    fetch(req)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error('NETWORK RESPONSE ERROR')
+        }
+      })
+      .then(data => {
+        //console.log(data)
+        let obj = data
+        //viewModel.set('message', getMessage(val))
+        let result
+        printValues(obj)
+        page.bindingContext.set('rezultat', getMessage(result))
+        console.log(result)
+
+        function printValues (obj) {
+          for (var k in obj) {
+            if (obj[k] instanceof Object) {
+              printValues(obj[k])
+            } else {
+              if (obj[k] != '' && obj[k] != undefined) {
+                //viewModel.set('message', getMessage("test"))
+                result += '<br>' + obj[k]
+              }
+            }
+          }
+        }
+      })
+      .catch(error => console.error('FETCH ERROR:', error))
+  })
 }
 
-export function onDrawerButtonTap(args) {
+export function onDrawerButtonTap (args) {
   const sideDrawer = Application.getRootView()
   sideDrawer.showDrawer()
 }
 
-const years = ["Predmet pravni","Predmet Fizički", "Stranka Pravna", "Strnaka Fizička"]
+const pretraga = [
+  'Predmet pravni',
+  'Predmet Fizički',
+  'Stranka Pravna',
+  'Stranka Fizička'
+]
 
 
+function getMessage (val) {
+  if (val !== '') {
+    return val
+  } else {
+    return '...'
+  }
+}
 
-
+let val = ''
 // export function onNavigatingTo(args: EventData) {
 //   const page = args.object
 //   const vm = fromObject({
