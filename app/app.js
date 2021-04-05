de092e761ed5f27a5a1260a47d3677cb6b8666dc
Kiki <kiki.ikik@gmail.com>
2021-04-06 00:20:46 +0200

    dodane prve notifikacije

--- app/app.js
+++ app/app.js
@@ -1,5 +1,25 @@
 import { Application } from '@nativescript/core'
 
+
+var firebase = require("@nativescript/firebase").firebase;
+firebase.init({
+    // Optionally pass in properties for database, authentication and cloud messaging,
+    // see their respective docs.
+    onMessageReceivedCallback: function(message) {
+        console.log(message)
+        console.log("Title: " + message.title);
+        console.log("Body: " + message.body);
+        // if your server passed a custom property called 'foo', then do this:
+        console.log("Value of 'foo': " + message.data.foo);
+      }
+  }).then(
+      function () {
+        console.log("firebase.init done");
+      },
+      function (error) {
+        console.log("firebase.init error: " + error);
+      }
+  );
 Application.run({ moduleName: 'app-root/app-root' })
 
 /*
