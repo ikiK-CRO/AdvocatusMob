d4b5343d35dbb159b90168accbf6be94c9e5a951
Kiki <kiki.ikik@gmail.com>
2021-04-14 19:50:07 +0200

    -a

--- app/home/home-page.js
+++ app/home/home-page.js
@@ -1,7 +1,12 @@
-import { Frame, Application } from '@nativescript/core'
+import { Frame, Application} from '@nativescript/core'
 import { HomeViewModel } from './home-view-model'
 import { inboxMeniIkona } from '~/app'
-//import { BarcodeScanner } from 'nativescript-barcodescanner'
+
+
+//import { BarcodeScanner } from "nativescript-barcodescanner";
+var BarcodeScanner = require("nativescript-barcodescanner").BarcodeScanner;
+let  barcodescanner = new BarcodeScanner();
+
 
 export function onNavigatingTo (args) {
   const page = args.object
@@ -72,34 +77,49 @@ export function onDrawerButtonTap (args) {
   sideDrawer.showDrawer()
 }
 
-export function scanBarcode () {
-  var BarcodeScanner = require('nativescript-barcodescanner').BarcodeScanner
-  console.log('csan tap')
-  var barcodescanner = new BarcodeScanner()
-  //barcodescanner.scan(false, true)
 
-  barcodescanner.scan({
-    formats: "QR_CODE,PDF_417",   // Pass in of you want to restrict scanning to certain types
-    //cancelLabel: "EXIT. Also, try the volume buttons!", // iOS only, default 'Close'
-    //cancelLabelBackgroundColor: "#333333", // iOS only, default '#000000' (black)
-    message: "Use the volume buttons for extra light", // Android only, default is 'Place a barcode inside the viewfinder rectangle to scan it.'
-    //showFlipCameraButton: true,   // default false
-    //preferFrontCamera: false,     // default false
-    //showTorchButton: true,        // default false
-    beepOnScan: true,             // Play or Suppress beep on scan (default true)
-    //fullScreen: true,             // Currently only used on iOS; with iOS 13 modals are no longer shown fullScreen by default, which may be actually preferred. But to use the old fullScreen appearance, set this to 'true'. Default 'false'.
-    //torchOn: false,               // launch with the flashlight on (default false)
-    closeCallback: function () { console.log("Scanner closed"); } // invoked when the scanner was closed (success or abort)
-    //resultDisplayDuration: 500,   // Android only, default 1500 (ms), set to 0 to disable echoing the scanned text
-    //orientation: "landscape",     // Android only, optionally lock the orientation to either "portrait" or "landscape"
-    //openSettingsIfPermissionWasPreviouslyDenied: true // On iOS you can send the user to the settings app if access was previously denied
-  }).then(
-      function(result) {
-        console.log("Scan format: " + result.format);
-        console.log("Scan text:   " + result.text);
-      },
-      function(error) {
-        console.log("No scan: " + error);
+
+export function scanBarcode () {
+
+  requestPermission().then((result) => {
+    //barcodescanner.scan(true, false);
+
+    barcodescanner.scan({
+          //cancelLabel: "Stop scanning",
+          //message: "Go scan something",
+          //preferFrontCamera: false,
+          showFlipCameraButton: false
+      }).then((result) => {
+          console.log("Scan format: " + result.format);
+          console.log("Scan text:   " + result.text);
+      }, (error) => {
+          console.log("No scan: " + error);
+      });
+  }, (error) => {
+      console.log("ERROR", error);
+    });
+}
+
+
+export function requestPermission() {
+  return new Promise((resolve, reject) => {
+    barcodescanner.available().then((available) => {
+      if(available) {
+        barcodescanner.hasCameraPermission().then((granted) => {
+              if(!granted) {
+                barcodescanner.requestCameraPermission().then(() => {
+                      resolve("Camera permission granted");
+                      console.log("Camera permission granted")
+                  });
+              } else {
+                  resolve("Camera permission was already granted");
+                  console.log("Camera permission was already granted")
+              }
+          });
+      } else {
+          reject("This device does not have an available camera");
+          console.log("This device does not have an available camera")
       }
-  );
+  });
+  });
 }
