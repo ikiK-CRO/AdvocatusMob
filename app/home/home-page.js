873ca06855e3bf7a90be6ae4bdc35d089cd9ab25
Kiki <kiki.ikik@gmail.com>
2021-04-15 14:24:16 +0200

    downgarde barcodescener fixed error

--- app/home/home-page.js
+++ app/home/home-page.js
@@ -3,9 +3,8 @@ import { HomeViewModel } from './home-view-model'
 import { inboxMeniIkona } from '~/app'
 
 
-//import { BarcodeScanner } from "nativescript-barcodescanner";
-var BarcodeScanner = require("nativescript-barcodescanner").BarcodeScanner;
-let  barcodescanner = new BarcodeScanner();
+import { BarcodeScanner } from "nativescript-barcodescanner";
+///var BarcodeScanner = require("nativescript-barcodescanner").BarcodeScanner;
 
 
 export function onNavigatingTo (args) {
@@ -77,26 +76,39 @@ export function onDrawerButtonTap (args) {
   sideDrawer.showDrawer()
 }
 
+let barcodescanner
+
+
 
 
 export function scanBarcode () {
-
+  barcodescanner = new BarcodeScanner();
   requestPermission().then((result) => {
     //barcodescanner.scan(true, false);
-
     barcodescanner.scan({
-          //cancelLabel: "Stop scanning",
-          //message: "Go scan something",
-          //preferFrontCamera: false,
-          showFlipCameraButton: false
-      }).then((result) => {
+      formats: "QR_CODE,PDF_417",   // Pass in of you want to restrict scanning to certain types
+      cancelLabel: "EXIT. Also, try the volume buttons!", // iOS only, default 'Close'
+      cancelLabelBackgroundColor: "#333333", // iOS only, default '#000000' (black)
+      message: "Use the volume buttons for extra light", // Android only, default is 'Place a barcode inside the viewfinder rectangle to scan it.'
+      showFlipCameraButton: true,   // default false
+      preferFrontCamera: false,     // default false
+      showTorchButton: true,        // default false
+      beepOnScan: true,             // Play or Suppress beep on scan (default true)
+      fullScreen: true,             // Currently only used on iOS; with iOS 13 modals are no longer shown fullScreen by default, which may be actually preferred. But to use the old fullScreen appearance, set this to 'true'. Default 'false'.
+      torchOn: false,               // launch with the flashlight on (default false)
+      closeCallback: function () { console.log("Scanner closed"); }, // invoked when the scanner was closed (success or abort)
+      resultDisplayDuration: 500,   // Android only, default 1500 (ms), set to 0 to disable echoing the scanned text
+      orientation: "portrait",     // Android only, optionally lock the orientation to either "portrait" or "landscape"
+      openSettingsIfPermissionWasPreviouslyDenied: true // On iOS you can send the user to the settings app if access was previously denied
+    }).then(
+        function(result) {
           console.log("Scan format: " + result.format);
           console.log("Scan text:   " + result.text);
-      }, (error) => {
+        },
+        function(error) {
           console.log("No scan: " + error);
-      });
-  }, (error) => {
-      console.log("ERROR", error);
+        }
+    );
     });
 }
 
